072e1f7826e457a74b335e3406576d6fcde68994
maribel <maribel.diaz.j@gmail.com>
2017-06-22 10:37:30 -0400

    trabajando en trazado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,22 @@
 function initMap(){
   var map = new google.maps.Map(document.getElementById("map"),{
     zoom: 5,
-    center: {lat: -9.1191427, lng: -77.0349046},
+    center: {lat: -33.43778, lng: -70.65028},
     mapTypeControl: false,
     zoomControl: false,
     streetViewControl: false
   });
 
 /*new AutocompleteDirectionsHandler(map);*/
+
+var partida = (document.getElementById("partida"));
+    var autocomplete = new google.maps.places.Autocomplete(partida);
+        autocomplete.bindTo("bounds", map);
+
+    var destino = (document.getElementById("destino"));
+    var autocomplete = new google.maps.places.Autocomplete(destino);
+        autocomplete.bindTo("bounds", map);
+  var infoWindow = new google.maps.InfoWindow({map: map});
 
 
 /* function getCurrentPosition - exito si el usuario comparte ubicacion - error */
@@ -16,6 +25,7 @@ function buscar(){
     navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
   }
 }
+
 document.getElementById("encuentrame").addEventListener("click", buscar);
 var latitud, longitud;
 
@@ -29,6 +39,7 @@ var funcionExito = function(posicion){
     animation: google.maps.Animation.DROP,
     map: map
   });
+
   /* aumentaremos profundidad de visualizacion del mapa (map.setZomm) y nuevo centro (map.setCenter) */
   map.setZoom(17);
   map.setCenter({lat:latitud, lng:longitud});
@@ -36,4 +47,26 @@ var funcionExito = function(posicion){
   var funcionError = function(error){
     alert("Tenemos un problema con encontrar tu ubicacion");
   }
+
+/* dibujando ruta*/
+function dibujarRuta(directionsService, directionsDisplay) {
+        var start = document.getElementById("partida").value;
+        var destination = document.getElementById('destino').value;
+
+       if(destination != "" && destination != "") {
+            directionsService.route({
+                start: start,
+                destination: destination,
+                travelMode: "DRIVING"
+            },
+            function(response, status) {
+                if (status === "OK") {
+                    directionsDisplay.setDirections(response);
+                } else {
+                    funcionErrorRuta();
+                }
+            });
+        }
+    }
+
 }
